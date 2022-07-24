4859cf0540f9862359085f6327df6dd368d20182
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-07-24 14:46:51 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+fetch('https://api.ipify.org?format=json')
+    .then(res => res.json())
+    .then(data => ipdetail(data.ip));
+
+const ipdetail = (getIp) => {
+    const ip = getIp + "";
+    fetch(`https://ipapi.co/${ ip }/json/`)
+        .then(res => res.json())
+        .then(data => handleDetails(data))
+        .catch(error => {
+            const detailsContainer = document.getElementById('details');
+            detailsContainer.innerHTML = `<h1> Something wrong! Please try again after some time!</h1>`;
+        });
+}
+
+const handleDetails = (data) => {
+    const detailsContainer = document.getElementById('details');
+    const details = document.createElement('div');
+    details.className = 'details-container'
+    details.innerHTML = `
+    <h1 class="m-2">Your Ip Address: ${data.ip}</h1>
+                <h3 class="m-1">Your Ip version: ${data.version}</h3>
+                <h3 class="m-1">City: ${data.city}</h3>
+                <h3 class="m-1">Region: ${data.region}</h3>
+                <h3 class="m-1">Region Code: ${data.region_code}</h3>
+                <h3 class="m-1">Country: ${data.country_name}</h3>
+                <h3 class="m-1">Country Code: ${data.country_code}</h3>
+                <h3 class="m-1">Postal Code: ${data.postal}</h3>
+                <h3 class="m-1">Latitude: ${data.latitude}</h3>
+                <h3 class="m-1">Longitude: ${data.longitude}</h3>
+                <h3 class="m-1">Timezone: ${data.timezone}</h3>
+                <h3 class="m-1">Currency: ${data.currency}</h3>
+                <h3 class="m-1">Country area: ${data.country_area}</h3>
+                <h3 class="m-1">Country Population: ${data.country_population}</h3>
+    `;
+    detailsContainer.appendChild(details);
+}
